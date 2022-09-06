3c95df7af8fe528c3ef6d85fddb09b8275f1729a
ParthParolekar <parolekarparth@gmail.com>
2022-09-06 21:49:56 +0530

    feat: chessboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import { Chessboard } from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  const columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const rows = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  let tempBoard = [];
+  const [knight, setKnight] = useState({
+    currentPosition: ["b", "1"],
+    possibleMoves: [
+      ["c", "3"],
+      ["a", "3"],
+      ["d", "2"],
+    ],
+  });
+  const [board, setBoard] = useState([]);
+
+  return (
+    <div className="App">
+      <Chessboard
+        columns={columns}
+        rows={rows}
+        tempBoard={tempBoard}
+        knight={knight}
+        setKnight={setKnight}
+        board={board}
+        setBoard={setBoard}
+      />
+    </div>
+  );
 }
 
 export default App;
